8c84e4ae7586f5aee0dad3fec1ceb01e934138b3
bfzq <bigbuggiggle@hotmail.com>
2020-02-29 01:47:23 +0800

    max profit

--- c/max_profit.c
+++ c/max_profit.c
@@ -2,13 +2,24 @@
 
 int maxProfit(int* prices, int pricesSize)
 {
-  // TODO
-  return 0;
+  int profit = 0;
+  if (pricesSize < 2)
+    return 0;
+  int front = 0, current = 1;
+  for (;current < pricesSize; current++)
+  {
+    if (prices[front] < prices[current])
+    {
+      profit += (prices[current] - prices[front]);
+    }
+    front = current;
+  }
+  return profit;
 }
 int main()
 {
   int prices[] = {7,1,5,3,6,4};
-  int profit = maxProfit(prices, sizeof(prices));
+  int profit = maxProfit(prices, 6);
   printf("%d", profit);
   return 0;
 }

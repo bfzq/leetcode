8f78d1accc1d8544df728d1a40eb18623f6c80ff
bfzq <bigbuggiggle@hotmail.com>
2020-02-29 01:24:31 +0800

    max profit

---
+++ c/max_profit.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int maxProfit(int* prices, int pricesSize)
+{
+  // TODO
+  return 0;
+}
+int main()
+{
+  int prices[] = {7,1,5,3,6,4};
+  int profit = maxProfit(prices, sizeof(prices));
+  printf("%d", profit);
+  return 0;
+}

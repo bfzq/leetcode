318764b33b0da74eca65589bae6987946c10b539
bfzq <bigbuggiggle@hotmail.com>
2020-03-06 01:13:48 +0800

    try

--- c/rotate.c
+++ c/rotate.c
@@ -1,26 +1,61 @@
 #include <stdio.h>
 
-// location(i) = (location(0) + i) % nums
-// location(0) = k
 void rotate1(int* nums, int numsSize, int k)
 {
-  int location = 0, current = 0, num = 0;
-  while ((location = (k + current) % numsSize) != 0)
+  printf("%d %d\n", numsSize, k);
+  k = k % numsSize;
+  int location = 0, current = 0, num = 0, x = 1, y;
+  for (int i = 0; i < x; i++)
   {
-    num = nums[location];
-    nums[location] = nums[0];
-    nums[0] = num;
-    current = location;
-  }
-  if ((numsSize % k) / 2 == 0)
-  {
-    location = 0, current = 1, num = 0;
-    while ((location = (k + current) % numsSize) != 1)
+    location = current = i;
+    while ((location = (k + current) % numsSize) != i)
     {
       num = nums[location];
-      nums[location] = nums[0];
-      nums[0] = num;
+      nums[location] = nums[i];
+      nums[i] = num;
       current = location;
+      for (int i = 0; i < numsSize; i++)
+        printf("%d,", nums[i]);
+      printf("\n");
+    }
+    printf("---\n");
+  }
+}
+
+void rotate3(int *nums, int numsSize, int k)
+{
+  int location = 0, current = 0, num = 0, count = 0;
+  for (int i = 0; i < k; i++)
+  {
+    current = i;
+    while ((location = (k + current) % numsSize) != i)
+    {
+      num = nums[location];
+      nums[location] = nums[i];
+      nums[i] = num;
+      current = location;
+      for (int i = 0; i < numsSize; i++)
+        printf("%d,", nums[i]);
+      printf("\n");
+      count++;
+    }
+    printf("\n");
+    if (count == (k % numsSize) - 1)
+      break;
+  }
+}
+
+void rotate2(int *nums, int numsSize, int k)
+{
+  if (numsSize < 2)
+    return;
+  for (int j = 0; j < k; j++)
+  {
+    for (int i = 0; i < numsSize; i++)
+    {
+      int num = nums[i + 1];
+      nums[i + 1] = nums[i];
+      
     }
   }
 }
@@ -28,9 +63,14 @@ void rotate1(int* nums, int numsSize, int k)
 
 int main()
 {
-  int nums[] = {1,2,3,4,5,6,7,8};
-  rotate1(nums, 8, 4);
-  for (int i = 0; i < 8; i++)
+  int nums[] = {1,2,3,4,5,6,7};
+  rotate3(nums, 7, 3);
+  for (int i = 0; i < 7; i++)
     printf("%d ", nums[i]);
+  printf("\n---\n");
+  int nums2[] = {1,2,3,4,5,6};
+  rotate3(nums2, 6, 4);
+  for (int i = 0; i < 6; i++)
+    printf("%d ", nums2[i]);
   return 0;
 }

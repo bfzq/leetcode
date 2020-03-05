366ecdd072a209c80988d6c84066126825c681c4
bfzq <bigbuggiggle@hotmail.com>
2020-03-06 01:14:01 +0800

    test official

--- official/jave/Rotate.java
+++ official/jave/Rotate.java
@@ -1,7 +1,7 @@
 public class Rotate {
   public static void main(String[] args) {
-    int nums[] = {1,2,3,4,5,6,7,8};
-    rotate(nums, 3);
+    int nums[] = {1,2,3,4,5,6};
+    rotate(nums, 4);
     for (int  i : nums) {
       System.out.println(i);
     }

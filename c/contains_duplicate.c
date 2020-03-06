29e29fb2d9ce503d41da1b13f61c31a2546e5f8a
bfzq <bigbuggiggle@hotmail.com>
2020-03-06 08:47:37 +0800

    timeout

--- c/contains_duplicate.c
+++ c/contains_duplicate.c
@@ -1,12 +1,24 @@
 #include <stdio.h>
+#include <stdbool.h>
 
-bool contains_duplicate(int *nums, int nusSize)
+/* timeout for */
+bool contains_duplicate(int *nums, int numsSize)
 {
-  return true;
+  for (int i = 0; i < numsSize; i++)
+  {
+    for (int j = i + 1; j < numsSize; j++)
+    {
+      if (nums[i] == nums[j])
+        return true;
+    }
+  }
+  return false;
 }
 
 
 int main()
 {
+  int nums1[] = {1,2,3,1};
+  printf("%d", contains_duplicate(nums1, 4));
   return 0;
 }

73a851ad517c22644bac614099f4cbc8061cf147
bfzq <bigbuggiggle@hotmail.com>
2020-03-07 00:43:58 +0800

    树

--- c/contains_duplicate.c
+++ c/contains_duplicate.c
@@ -15,10 +15,68 @@ bool contains_duplicate(int *nums, int numsSize)
   return false;
 }
 
-
+// binary tree
+struct node_struct
+{
+  int num;
+  struct node_struct *left, *right;
+};
+typedef struct node_struct node_t;
+bool contains_duplicate2(int *nums, int numsSize)
+{
+  node_t *head = NULL, *p = NULL;
+  for (int i = 0; i < numsSize; i++)
+  {
+    if (head == NULL)
+    {
+      node_t node;
+      node.num = nums[i];
+      node.left = node.right = NULL;
+      head = &node;
+    }
+    else
+    {
+      p = head;
+      while (p != NULL)
+      {
+        if (p->num < nums[i])
+        {
+          if (p->right != NULL)
+            p = p->right;
+          else
+          {
+            node_t node;
+            node.num = nums[i];
+            node.left = node.right = NULL;
+            p->right = &node;
+            break;
+          }
+        }
+        else if (p->num > nums[i])
+        {
+          if (p->left != NULL)
+            p = p->left;
+          else
+          {
+            node_t node;
+            node.num = nums[i];
+            node.left = node.right = NULL;
+            p->left = &node;
+            break;
+          }
+        }
+        else
+        {
+          return true;
+        }
+      }
+    }
+  }
+  return false;
+}
 int main()
 {
   int nums1[] = {1,2,3,1};
-  printf("%d", contains_duplicate(nums1, 4));
+  printf("%d", contains_duplicate2(nums1, 4));
   return 0;
 }

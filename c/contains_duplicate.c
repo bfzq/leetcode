e868eb0a9613d386ede0b68255ad503f15ae7f06
bfzq <bigbuggiggle@hotmail.com>
2020-03-11 00:15:09 +0800

    hash + linear probing but timeout

--- c/contains_duplicate.c
+++ c/contains_duplicate.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdbool.h>
-
+#include <stdlib.h>
+#include <string.h>
 /* timeout for */
 bool contains_duplicate(int *nums, int numsSize)
 {
@@ -75,27 +76,94 @@ bool contains_duplicate2(int *nums, int numsSize)
   return false;
 }
 
-// hash
+// hash + linear probing
+// timeout
 bool contains_duplicate3(int *nums, int numsSize)
 {
-  return true;
+  int **tbl = (int**)malloc(numsSize * sizeof(int*));
+  memset(tbl, 0, numsSize * sizeof(int*));
+  int idx = 0;
+  bool ret = false;
+  for (int i = 0; i < numsSize; i++)
+  {
+    idx = *(nums + i) % numsSize;
+    if (idx < 0) idx *= -1;
+    if (tbl[idx] == NULL)
+      tbl[idx] = &(nums[i]);
+    else if (*(tbl[idx]) == nums[i])
+    {
+      ret = true;
+    }
+    else
+    {
+      for (int j = (idx + 1) % numsSize; j != idx; j = (j + 1) % numsSize)
+      {
+        if (tbl[j] == NULL)
+        {
+          tbl[j] = &(nums[i]);
+          break;
+        }
+        else if (*(tbl[j]) == nums[i])
+        {
+          ret = true;
+          break;
+        }
+      }
+    }
+    if (ret == true)
+      break;
+  }
+  free(tbl);
+  return ret;
 }
 
 void test1(bool(*func)(int*, int))
 {
   int nums[] = {1, 2, 3, 1};
-  printf("%d\n", func(nums, 4));
+  printf("test1: %d\n", func(nums, 4));
 }
 
 void test2(bool(*func)(int*, int))
 {
   int nums[] = {1, 2, 3};
-  printf("%d\n", func(nums, 3));
+  printf("test2: %d\n", func(nums, 3));
 }
+
+void test3(bool(*func)(int*, int))
+{
+  int nums[] = {3, 1};
+  printf("test3: %d\n", func(nums, 2));
+}
+
+void test4(bool(*func)(int*, int))
+{
+  int nums[] = {1, 5, -2, -4, 0};
+  printf("test4: %d\n", func(nums, 5));
+}
+
+void test5(bool(*func)(int*, int))
+{
+  int nums[] = {2, 14, 18, 22, 22};
+  printf("test5: %d\n", func(nums, 5));
+}
+
+void test6(bool(*func)(int*, int))
+{
+  int nums[] = {-1200000005,-1200000005};
+  printf("test6: %d\n", func(nums, 2));
+}
+
+
 
 int main()
 {
-  test1(contains_duplicate);
-  test2(contains_duplicate);
+  /* test1(contains_duplicate); */
+  /* test2(contains_duplicate); */
+  test1(contains_duplicate3);
+  test2(contains_duplicate3);
+  test3(contains_duplicate3);
+  test4(contains_duplicate3);
+  test5(contains_duplicate3);
+  test6(contains_duplicate3);
   return 0;
 }

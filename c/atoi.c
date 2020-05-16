8596c86b1f86c97ac463f38ec11660b5c7805456
bfzq <bigbuggiggle@hotmail.com>
2020-05-16 17:29:53 +0800

    atoi init

---
+++ c/atoi.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int myAtoi(char *str)
+{
+  return 0;
+}
+
+void test(char * str)
+{
+  printf("%d\n", myAtoi(str));
+}
+
+int main()
+{
+  test("    -41");
+  test("410");
+  test("003");
+  test("003 ddds dd");
+  test("-91283472332");
+  return 0;
+}

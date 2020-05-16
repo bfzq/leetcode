dbeb2451a321f9b5ac1ade00df54ba271260cf7c
bfzq <bigbuggiggle@hotmail.com>
2020-05-16 17:25:52 +0800

    convert done

--- c/convert.c
+++ c/convert.c
@@ -4,6 +4,37 @@
 
 char* convert(char *s, int numRows)
 {
+  if (numRows == 1) return s;
+  int len = strlen(s);
+  char *s_ = (char*)malloc(len + 1);
+  char *s__ = s_;
+  int step = (numRows - 1) * 2;
+  for (int i = 0; i < numRows; i++)
+  {
+    int j = 0;
+    while (1)
+    {
+      if (j == 0 || i == numRows - 1 || i == 0)
+      {
+        if (j + i >= len) break;
+        *s_ = s[j + i];
+        s_++;
+      }
+      else
+      {
+        if (j - i >= len) break;
+        *s_ = s[j - i];
+        s_++;
+        if (j + i >= len) break;
+        *s_ = s[j + i];
+        s_++;
+      }
+      j += step;
+    }
+  }
+  s__[len] = 0;
+  strcpy(s, s__);
+  free(s__);
   return s;
 }
 
@@ -19,7 +50,10 @@ void test(char *s, int numRows)
 
 int main()
 {
+  test("LEETCODEISHIRING", 1);
+  test("LEETCODEISHIRING", 2);
   test("LEETCODEISHIRING", 3);
   test("LEETCODEISHIRING", 4);
+  test("LEETCODEISHIRING", 16);
   return 0;
 }

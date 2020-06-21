ccf52d4ef31716afb9243ffbc9710c7848134f3b
bfzq <bigbuggiggle@hotmail.com>
2020-06-21 22:04:14 +0800

    单调栈用法2

--- c/largest_rectangle_area.cc
+++ c/largest_rectangle_area.cc
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 // timeout
 int largestRectangleArea(int* heights, int heightsSize)
 {
@@ -40,26 +41,59 @@ int largestRectangleArea(int* heights, int heightsSize)
 // 单调栈
 int largestRectangleArea2(int *heights, int heightsSize)
 {
+  // heightsSize++;
+  // int *stack = (int*)malloc(heightsSize*sizeof(int));
+  // memset(stack,0,heightsSize*sizeof(int));
+  // int *cpyHi = (int*)malloc(heightsSize*sizeof(int));
+  // memcpy(cpyHi,heights,(heightsSize-1)*sizeof(int));
+  // cpyHi[heightsSize-1]=0;
+  // int top = 0;
+  // int maxArea = 0;
+  // int left=0;
+  // int right = 0;
+  // for(int i=0;i<heightsSize;i++){
+  //   while(top!=0 && cpyHi[stack[top-1]]>cpyHi[i]){
+  //     int h = cpyHi[stack[top-1]];
+  //     top--;
+  //     int leftest = (top==0?-1:stack[top-1]);
+  //     int area = h * (i-leftest-1);
+  //     if(area>maxArea){
+  //       maxArea = area;
+  //     }
+  //   }
+  //   stack[top] = i;
+  //   top++;
+  // }
+  // return maxArea;
+ 
+  if (heightsSize == 0) return 0;
   int area = 0, idx = 0;
-  int *stack = (int*)malloc(heightsSize + 2);
-  
+  int *stack = (int*)malloc(heightsSize * sizeof(int));
+  memset(stack, 0, heightsSize * sizeof(int));
   stack[0] = 0;
-  stack[heightsSize + 1] = 0;
-  
-  for (int i = 1; i < heightsSize; i++)
+  for (int i = 1; i < heightsSize + 1; i++)
   {
-    while (idx >= 1 && stack[idx] < heights[i])
+    while (idx >= 0 &&
+           (i == heightsSize || heights[stack[idx]] > heights[i]))
     {
-      
+      int height = heights[stack[idx]];
+
+      int left = idx == 0 ? 0 : stack[idx];
+      idx--;
+      int right = i;
+      int tmp = height * (right - left);
+      if (tmp > area) area = tmp;
     }
-    stack[++idx] = heights[i];
+    stack[++idx] = i;
   }
+  free(stack);
   return area;
+  
 }
 
 void test(int *h, int hs)
 {
-  printf("%d\n", largestRectangleArea(h, hs));
+  printf("%d\n", largestRectangleArea2(h, hs));
 }
     
 
@@ -69,9 +103,13 @@ int main()
   test(t1, sizeof(t1) / sizeof(int));
   int t2[] = {0};
   test(t2, sizeof(t2) / sizeof(int));
-  int t3[] = {2,0,2};
+  int t3[] = {2,1,2};
   test(t3, sizeof(t3) / sizeof(int));
-  int t4[] = {2,1,2};
+  int t4[] = {2,3,1,1,1};
   test(t4, sizeof(t4) / sizeof(int));
+  int t5[] = {};
+  test(t5, sizeof(t5) / sizeof(int));
+  int t6[] = {4,2,0,3,2,5};
+  test(t6, sizeof(t6) / sizeof(int));
   return 0;
 }

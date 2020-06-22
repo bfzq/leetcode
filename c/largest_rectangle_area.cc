6ea0bf0e24624c58b0fc199dffb2ce2f22b4cdaf
bfzq <bigbuggiggle@hotmail.com>
2020-06-22 22:02:09 +0800

    单调栈用法3

--- c/largest_rectangle_area.cc
+++ c/largest_rectangle_area.cc
@@ -41,54 +41,29 @@ int largestRectangleArea(int* heights, int heightsSize)
 // 单调栈
 int largestRectangleArea2(int *heights, int heightsSize)
 {
-  // heightsSize++;
-  // int *stack = (int*)malloc(heightsSize*sizeof(int));
-  // memset(stack,0,heightsSize*sizeof(int));
-  // int *cpyHi = (int*)malloc(heightsSize*sizeof(int));
-  // memcpy(cpyHi,heights,(heightsSize-1)*sizeof(int));
-  // cpyHi[heightsSize-1]=0;
-  // int top = 0;
-  // int maxArea = 0;
-  // int left=0;
-  // int right = 0;
-  // for(int i=0;i<heightsSize;i++){
-  //   while(top!=0 && cpyHi[stack[top-1]]>cpyHi[i]){
-  //     int h = cpyHi[stack[top-1]];
-  //     top--;
-  //     int leftest = (top==0?-1:stack[top-1]);
-  //     int area = h * (i-leftest-1);
-  //     if(area>maxArea){
-  //       maxArea = area;
-  //     }
-  //   }
-  //   stack[top] = i;
-  //   top++;
-  // }
-  // return maxArea;
- 
   if (heightsSize == 0) return 0;
-  int area = 0, idx = 0;
+  int area = 0, top = 0;
   int *stack = (int*)malloc(heightsSize * sizeof(int));
   memset(stack, 0, heightsSize * sizeof(int));
-  stack[0] = 0;
+  stack[top] = 0;
   for (int i = 1; i < heightsSize + 1; i++)
   {
-    while (idx >= 0 &&
-           (i == heightsSize || heights[stack[idx]] > heights[i]))
+    while (top >= 0 &&
+           (i == heightsSize || heights[stack[top]] > heights[i]))
     {
-      int height = heights[stack[idx]];
+      int height = heights[stack[top]];
 
-      int left = idx == 0 ? 0 : stack[idx];
-      idx--;
-      int right = i;
+      /* stack 左边空缺的值的都是大于当前的值 */
+      int left = top-- == 0 ? -1 : stack[top];
+      
+      int right = i - 1;
       int tmp = height * (right - left);
       if (tmp > area) area = tmp;
     }
-    stack[++idx] = i;
+    stack[++top] = i;
   }
   free(stack);
   return area;
-  
 }
 
 void test(int *h, int hs)
@@ -109,7 +84,7 @@ int main()
   test(t4, sizeof(t4) / sizeof(int));
   int t5[] = {};
   test(t5, sizeof(t5) / sizeof(int));
-  int t6[] = {4,2,0,3,2,5};
+  int t6[] = {0,3,2,5};
   test(t6, sizeof(t6) / sizeof(int));
   return 0;
 }

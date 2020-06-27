d6d911be1cc392cb3ef8ac348b7adb9232449a1f
bfzq <bigbuggiggle@hotmail.com>
2020-06-27 09:27:41 +0800

    search

--- c/findmediansortedarrays.cc
+++ c/findmediansortedarrays.cc
@@ -2,6 +2,12 @@
 double findMedianSortedArrays(int* nums1, int nums1Size,
                               int* nums2, int nums2Size)
 {
+  int left_total_element_num = (nums1Size + nums2Size + 1) / 2;
+  int left_element_num1 = 0, left_element_num2 = 0;
+  while (left_element_num1 + left_element_num2 < left_total_element_num)
+  {
+    
+  }
   return 0;
 }
 
---
+++ c/search.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int search(int* nums, int numsSize, int target)
+{
+  
+}
+
+int main()
+{
+  return 0;
+}

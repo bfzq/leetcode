65bbc1b998da3fc52d98904989dd08f32b481e15
bfzq <bigbuggiggle@hotmail.com>
2020-06-27 23:50:05 +0800

    二分查找 init

--- c/findmediansortedarrays.h
+++ c/findmediansortedarrays.h
@@ -4,8 +4,70 @@
 double findMedianSortedArrays(int* nums1, int nums1Size,
                               int* nums2, int nums2Size)
 {
-  double median_value;
-  return median_value;
+  int left_median_element_num = (nums1Size + nums2Size + 1) / 2;
+  int left_element_num1 = 0, left_element_num2 = 0;
+  int mid = 0;
+  while (left_element_num1 + left_element_num2 != left_median_element_num)
+  {
+    if (nums1[left_element_num1] <= nums2[left_element_num2])
+    {
+      while ((mid = (left_element_num1 + nums1Size) / 2) + left_element_num2 >
+             left_median_element_num ||
+             nums1[mid] > nums2[left_element_num2])
+      {
+        mid = (left_element_num1 + mid) / 2;
+      }
+      left_element_num1 = mid + 1;
+    }
+    else
+    {
+      while ((mid = (left_element_num2 + nums2Size) / 2) + left_element_num1 >
+             left_median_element_num ||
+             nums2[mid] > nums1[left_element_num1])
+      {
+        mid = (left_element_num2 + mid) / 2;
+      }
+      left_element_num2 = mid + 1;
+    }
+  }
+  if (left_element_num1 == 0 && left_element_num2 == 0) return 0;
+  double left_value = 0;
+  if (left_element_num1 != 0 && left_element_num2 == 0)
+  {
+    left_value = nums1[left_element_num1 - 1];
+  }
+  else if (left_element_num1 == 0 && left_element_num2 != 0)
+  {
+    left_value = nums2[left_element_num2 - 1];
+  }
+  else
+  {
+    left_value = nums1[left_element_num1 - 1] > nums2[left_element_num2 - 1] ?
+      nums1[left_element_num1 - 1] : nums2[left_element_num2 - 1];
+  }
+
+  if ((nums1Size + nums2Size) % 2 == 0)
+  {
+    double right_value = 0;
+    if (left_element_num1 == nums1Size)
+    {
+      right_value = nums2[left_element_num2];
+    }
+    else if (left_element_num2 == nums2Size)
+    {
+      right_value = nums1[left_element_num1];
+    }
+    else
+    {
+      right_value = nums1[left_element_num1] < nums2[left_element_num2] ?
+        nums1[left_element_num1] : nums2[left_element_num2];
+    }
+    return (left_value + right_value) / 2;
+  }
+  else
+  {
+    return left_value;
+  }
 }
 
 #endif // _FINDMEDIANSORTEDARRAYS_H_

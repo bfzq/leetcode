785e22659413f23713fa2923ed4ff79c27bd7c8b
bfzq <bigbuggiggle@hotmail.com>
2020-06-27 21:47:56 +0800

    gtest

---
+++ c/findmediansortedarrays.h
@@ -0,0 +1,11 @@
+#ifndef _FINDMEDIANSORTEDARRAYS_H_
+#define _FINDMEDIANSORTEDARRAYS_H_
+
+double findMedianSortedArrays(int* nums1, int nums1Size,
+                              int* nums2, int nums2Size)
+{
+  double median_value;
+  return median_value;
+}
+
+#endif // _FINDMEDIANSORTEDARRAYS_H_
--- c/test.cc
+++ c/test.cc
@@ -1,4 +1,5 @@
 #include <gtest/gtest.h>
+#include "findmediansortedarrays.h"
 
 // g++ -std=c++11 -g test.cc -o test -I/usr/local/include -L/usr/local/lib -lgtest
 
@@ -12,6 +13,15 @@ TEST(test, max)
 {
   EXPECT_EQ(2, max(2,-1));
   EXPECT_EQ(3, max(2,3));
+}
+
+TEST(leetcode, findMedianSortedArrays)
+{
+  int nums1[] = {1, 3};
+  int nums2[] = {2};
+  EXPECT_EQ(2.0,
+            findMedianSortedArrays(nums1, sizeof(nums1) / sizeof(int),
+                                   nums2, sizeof(nums2) / sizeof(int)));
 }
 
 int main(int argc, char **argv)

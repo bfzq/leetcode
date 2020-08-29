5a97255e5d162fb7639805a0946b7a46a7ab8e62
bfzq <bigbuggiggle@hotmail.com>
2020-08-29 20:17:01 +0800

    add makefile add maxArea

--- c++/main.cc
+++ c++/main.cc
@@ -1,3 +1,4 @@
+#include <gtest/gtest.h>
 #include "romanToInt.h"
 #include <iostream>
 int main()
---
+++ c++/solution.h
@@ -0,0 +1,18 @@
+#ifndef _LEETCODE_SOLUTION_
+#define _LEETCODE_SOLUTION_
+
+class Solution
+{
+ public:
+  int maxArea(vector<int>& height)
+  {
+    return maxArea1(height);
+  }
+ private:
+  int maxArea1(vector<int> &height)
+  {
+    return 0;
+  }
+};
+
+#endif // _LEETCODE_SOLUTION_
